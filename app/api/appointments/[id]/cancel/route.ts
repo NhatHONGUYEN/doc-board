fa6941f1bc3f09fad465afbff9a3a62731681a14
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-03-14 10:53:49 +0100

    🐛   F04 :  try to fix probleme with datepicker

--- app/api/appointments/[id]/cancel/route.ts
+++ app/api/appointments/[id]/cancel/route.ts
@@ -6,7 +6,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function PUT(
-  request: NextRequest,
+  _request: NextRequest,
   context: { params: { id: string } }
 ) {
   try {
