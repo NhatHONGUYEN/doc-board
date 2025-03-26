3d13ce93cf28ff1287c8335e889e8e86f4695c2f
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-03-26 11:28:22 +0100

    ✨  F011 : Preapre my landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Faq from "@/components/Faq";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+
 import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Features />
+      <Faq />
+      <Pricing />
+      <Footer />
     </>
   );
 }
